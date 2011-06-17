8758f61989db95c3c6902927784d6108825dc993
Greg Luck <gluck@gregluck.com>
2011-06-17 12:42:40 +1000

    Change example to JUnit.

--- src/main/java/org/terracotta/TerracottaCloudFormationSample.java
+++ src/main/java/org/terracotta/TerracottaCloudFormationSample.java
@@ -37,64 +37,69 @@ import com.amazonaws.services.cloudformation.model.StackResource;
 /**
  * This sample demonstrates how to make basic requests to AWS CloudFormation
  * using the AWS SDK for Java.
- * <p>
+ * <p/>
  * <b>Prerequisites:</b> You must have a valid Amazon Web Services developer
  * account, and be signed up to use AWS CloudFormation. For more information on
  * AWS CloudFormation, see http://aws.amazon.com/cloudformation.
- * <p>
+ * <p/>
  * <b>Important:</b> Be sure to fill in your AWS access credentials in the
- *                   AwsCredentials.properties file before you try to run this
- *                   sample.
+ * AwsCredentials.properties file before you try to run this
+ * sample.
  * http://aws.amazon.com/security-credentials
  */
-public class CloudFormationSample {
-
-    public static void main(String[] args) throws Exception {
-         /*
-         * Important: Be sure to fill in your AWS access credentials in the
-         *            AwsCredentials.properties file before you try to run this
-         *            sample.
-         * http://aws.amazon.com/security-credentials
-         */
-        AmazonCloudFormation stackbuilder = new AmazonCloudFormationClient(new PropertiesCredentials(
-                CloudFormationSample.class.getResourceAsStream("/AwsCredentials.properties")));
+public class TerracottaCloudFormationSample {
 
 
-        System.out.println("===========================================");
-        System.out.println("Getting Started with AWS CloudFormation");
-        System.out.println("===========================================\n");
+    private static String stackName = "TerracottaCloudFormationSampleStack";
+    private static String logicalResourceName = "SampleNotificationTopic";
+    private static String keyChainName = "gluck"; //change this to the key chain you want to use to ssh into instances.
+    private static AmazonCloudFormation amazonCloudFormationClient;
 
-        String stackName           = "CloudFormationSampleStack";
-        String logicalResourceName = "SampleNotificationTopic";
+    @org.junit.BeforeClass
+    public static void createStack() throws Exception {
+
+        /*
+        * Important: Be sure to fill in your AWS access credentials in the
+        *            AwsCredentials.properties file before you try to run this
+        *            sample.
+        * http://aws.amazon.com/security-credentials
+        */
+        amazonCloudFormationClient = new AmazonCloudFormationClient(new PropertiesCredentials(
+                TerracottaCloudFormationSample.class.getResourceAsStream("/AwsCredentials.properties")));
+
+
+        System.out.println("================================");
+        System.out.println("Terracotta CloudFormation Sample");
+        System.out.println("================================\n");
+
 
         try {
             // Create a stack
             CreateStackRequest createRequest = new CreateStackRequest();
             createRequest.setStackName(stackName);
-//            createRequest.setTemplateBody(convertStreamToString(CloudFormationSample.class.getResourceAsStream("/CloudFormationSample.template")));
-            createRequest.setTemplateBody(convertStreamToString(CloudFormationSample.class.getResourceAsStream("/SampleRailsApp.template")));
+            createRequest.setTemplateBody(convertStreamToString(TerracottaCloudFormationSample.class.getResourceAsStream("/TerracottaServerArray.template")));
 
 
             Parameter parameter = new Parameter();
             parameter.setParameterKey("KeyName");
-            parameter.setParameterValue("this_should_be_your_pair_name");
+            parameter.setParameterValue(keyChainName);
             List parameters = new ArrayList();
             parameters.add(parameter);
             createRequest.setParameters(parameters);
             System.out.println("Creating a stack called " + createRequest.getStackName() + ".");
-            stackbuilder.createStack(createRequest);
+            amazonCloudFormationClient.createStack(createRequest);
 
             // Wait for stack to be created
             // Note that you could use SNS notifications on the CreateStack call to track the progress of the stack creation
-            System.out.println("Stack creation completed, the stack " + stackName + " completed with " + waitForCompletion(stackbuilder, stackName));
+            System.out.println("Stack creation completed, the stack " + stackName + " completed with " + waitForCompletion(amazonCloudFormationClient, stackName));
 
             // Show all the stacks for this account along with the resources for each stack
-            for (Stack stack : stackbuilder.describeStacks(new DescribeStacksRequest()).getStacks()) {
+            for (Stack stack : amazonCloudFormationClient.describeStacks(new DescribeStacksRequest()).getStacks()) {
                 System.out.println("Stack : " + stack.getStackName() + " [" + stack.getStackStatus().toString() + "]");
 
                 DescribeStackResourcesRequest stackResourceRequest = new DescribeStackResourcesRequest();
                 stackResourceRequest.setStackName(stack.getStackName());
-                for (StackResource resource : stackbuilder.describeStackResources(stackResourceRequest).getStackResources()) {
+                for (StackResource resource : amazonCloudFormationClient.describeStackResources(stackResourceRequest).getStackResources()) {
                     System.out.format("    %1$-40s %2$-25s %3$s\n", resource.getResourceType(), resource.getLogicalResourceId(), resource.getPhysicalResourceId());
                 }
             }
@@ -104,19 +109,11 @@ public class CloudFormationSample {
             logicalNameResourceRequest.setStackName(stackName);
             logicalNameResourceRequest.setLogicalResourceId(logicalResourceName);
             System.out.format("Looking up resource name %1$s from stack %2$s\n", logicalNameResourceRequest.getLogicalResourceId(), logicalNameResourceRequest.getStackName());
-            for (StackResource resource : stackbuilder.describeStackResources(logicalNameResourceRequest).getStackResources()) {
+            for (StackResource resource : amazonCloudFormationClient.describeStackResources(logicalNameResourceRequest).getStackResources()) {
                 System.out.format("    %1$-40s %2$-25s %3$s\n", resource.getResourceType(), resource.getLogicalResourceId(), resource.getPhysicalResourceId());
             }
 
-            // Delete the stack
-            DeleteStackRequest deleteRequest = new DeleteStackRequest();
-            deleteRequest.setStackName(stackName);
-            System.out.println("Deleting the stack called " + deleteRequest.getStackName() + ".");
-            stackbuilder.deleteStack(deleteRequest);
-
-            // Wait for stack to be deleted
-            // Note that you could used SNS notifications on the original CreateStack call to track the progress of the stack deletion
-            System.out.println("Stack creation completed, the stack " + stackName + " completed with " + waitForCompletion(stackbuilder, stackName));
+            //deleteStack
 
         } catch (AmazonServiceException ase) {
             System.out.println("Caught an AmazonServiceException, which means your request made it "
@@ -141,11 +138,11 @@ public class CloudFormationSample {
         StringBuilder stringbuilder = new StringBuilder();
         String line = null;
         while ((line = reader.readLine()) != null) {
-          stringbuilder.append(line + "\n");
+            stringbuilder.append(line + "\n");
         }
         in.close();
         return stringbuilder.toString();
-      }
+    }
 
     // Wait for a stack to complete transitioning
     // End stack states are:
@@ -159,16 +156,15 @@ public class CloudFormationSample {
         DescribeStacksRequest wait = new DescribeStacksRequest();
         wait.setStackName(stackName);
         Boolean completed = false;
-        String  stackStatus = "Unknown";
-        String  stackReason = "";
+        String stackStatus = "Unknown";
+        String stackReason = "";
 
         System.out.print("Waiting");
 
         while (!completed) {
             List<Stack> stacks = stackbuilder.describeStacks(wait).getStacks();
-            if (stacks.isEmpty())
-            {
-                completed   = true;
+            if (stacks.isEmpty()) {
+                completed = true;
                 stackStatus = "NO_SUCH_STACK";
                 stackReason = "Stack has been deleted";
             } else {
@@ -187,8 +183,8 @@ public class CloudFormationSample {
             // Show we are waiting
             System.out.print(".");
 
-            // Not done yet so sleep for 10 seconds.
-            if (!completed) Thread.sleep(10000);
+            // Not done yet so sleep for 30 seconds.
+            if (!completed) Thread.sleep(30000);
         }
 
         // Show we are done
@@ -196,4 +192,31 @@ public class CloudFormationSample {
 
         return stackStatus + " (" + stackReason + ")";
     }
+
+    @org.junit.AfterClass
+    public static void deleteStack() throws Exception {
+        // Delete the stack
+            DeleteStackRequest deleteRequest = new DeleteStackRequest();
+            deleteRequest.setStackName(stackName);
+            System.out.println("Deleting the stack called " + deleteRequest.getStackName() + ".");
+            amazonCloudFormationClient.deleteStack(deleteRequest);
+
+            // Wait for stack to be deleted
+            // Note that you could used SNS notifications on the original CreateStack call to track the progress of the stack deletion
+            System.out.println("Stack creation completed, the stack " + stackName + " completed with " + waitForCompletion(amazonCloudFormationClient, stackName));
+    }
+
+    @org.junit.Test
+    public void testStackCreated() {
+                    // Lookup a resource by its logical name
+            DescribeStackResourcesRequest logicalNameResourceRequest = new DescribeStackResourcesRequest();
+            logicalNameResourceRequest.setStackName(stackName);
+            logicalNameResourceRequest.setLogicalResourceId(logicalResourceName);
+            System.out.format("Looking up resource name %1$s from stack %2$s\n", logicalNameResourceRequest.getLogicalResourceId(), logicalNameResourceRequest.getStackName());
+            for (StackResource resource : amazonCloudFormationClient.describeStackResources(logicalNameResourceRequest).getStackResources()) {
+                System.out.format("    %1$-40s %2$-25s %3$s\n", resource.getResourceType(), resource.getLogicalResourceId(), resource.getPhysicalResourceId());
+            }
+
+    }
+
 }
